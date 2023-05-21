788085c191d815c83d91e2ac4dc5848dac668535
zzzzz0001 <86714683+zzzzz0001@users.noreply.github.com>
2023-05-21 16:40:31 +0530

    Update Scala_version_2.12.15.sh
    Changed the way to do the command "sudo rm" in the file Scala_version_2.12.15.sh.

--- Scala_version_2.12.15.sh
+++ Scala_version_2.12.15.sh
@@ -2,6 +2,10 @@
 
 #scala version 2.12.15
 
+#eclipse website to install - https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2023-03/R/eclipse-inst-jre-linux64.tar.gz&mirror_id=1135
+#jetbrains toolbox to install - https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
+#jetbrains sbt version for project choosing - https://stackoverflow.com/questions/49000201/whats-the-relationship-of-the-versions-of-scala-when-i-use-sbt-to-build-a-scala
+
 #CHECKING IF YOU WANT TO ACTUALLY RUN IT OR NOT
 #-------------------------------------------------
 while true
@@ -115,18 +119,28 @@ done
 scala_version='2.12.15'
 scala_foldername="scala-$scala_version"
 
-#DOWNLOADING SCALA
+#DOWNLOADING SCALA FROM https://www.scala-lang.org/download/ go into https://www.scala-lang.org/download/2.12.15.html and extract to ~
 #-----------------------------------------------------------------
 while true
 do
-  echo "INSTALLATION METHOD"
+    echo "INSTALLATION METHOD"
 	echo "1. DEB FILE INSTALATION (recommended)"
 	echo "2. APT BASED INSTALLATION"
-	echo "3. BINARY FILE INSTALATION"
+	echo "3. BINARY FILE INSTALATION"   #https://downloads.lightbend.com/scala/2.12.15/scala-2.12.15.tgz     https://www.scala-lang.org/download/install.html
     
 	read installation_method
     if [ "$installation_method" == "1" ]; then
-        sudo rm ~/Downloads/$scala_foldername.deb
+
+        cd ~/Downloads
+        if [ $(find -maxdepth 1 | grep $scala_foldername.deb) ]; then
+            sudo rm $scala_foldername.deb
+            echo "COPY OF SCALA.DEB DELETED. CONTINUING"
+        else
+            echo "SCALA.DEB FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+        cd ~
+
+        
 		wget -P ~/Downloads https://downloads.lightbend.com/scala/$scala_version/$scala_foldername.deb
 		sudo apt install ~/Downloads/$scala_foldername.deb
         break
@@ -141,7 +155,17 @@ do
 		exit 0
         
     elif [ "$installation_method" == "3" ]; then
-        sudo rm ~/Downloads/$scala_foldername.tgz
+
+        cd ~/Downloads
+        if [ $(find -maxdepth 1 | grep $scala_foldername.tgz) ]; then
+            sudo rm $scala_foldername.tgz
+            echo "COPY OF SCALA.TGZ DELETED. CONTINUING"
+        else
+            echo "JETBRAINS TOOLS FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+        cd ~
+
+        
         wget -P ~/Downloads https://downloads.lightbend.com/scala/$scala_version/$scala_foldername.tgz
         tar -zxvf ~/Downloads/$scala_foldername.tgz
 		break
@@ -168,7 +192,16 @@ do
     echo "Want to install eclipse 2023? ((y)es / (n)o)"
 	read answer
     if [ "$answer" == "yes" ] || [ "$answer" == "y" ]; then
-        sudo rm ~/Downloads/eclipse-inst-jre-linux64.tar.gz
+
+        cd ~/Downloads
+        if [ $(find -maxdepth 1 | grep eclipse-inst-jre-linux) ]; then
+            sudo rm eclipse-inst-jre-linux64.tar.gz
+            echo "COPY OF ECLIPSE TOOLS DELETED. CONTINUING"
+        else
+            echo "ECLIPSE FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+        cd ~
+
         firefox https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2023-03/R/eclipse-inst-jre-linux64.tar.gz\&mirror_id\=1135
 
         echo "WARNING, WAIT FOR THE FILE TO BE FULLY DOWNLOADED BEFORE YOU HIT ANY KEY"
@@ -196,7 +229,15 @@ do
     echo "Want to install jetbrains toolbox (to install intellij)? ((y)es / (n)o)"
 	read answer
     if [ "$answer" == "yes" ] || [ "$answer" == "y" ]; then
-        sudo rm ~/Downloads/jetbrains-toolbox-1.28.1.15219.tar.gz
+        cd ~/Downloads
+        if [ $(find -maxdepth 1 | grep jetbrains-toolbox) ]; then
+            sudo rm jetbrains-toolbox-1.28.1.15219.tar.gz
+            echo "COPY OF JETBRAINS TOOLS DELETED. CONTINUING"
+        else
+            echo "JETBRAINS TOOLS FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+        cd ~
+
         firefox https://www.jetbrains.com/toolbox-app/download/download-thanks.html?platform=linux
 
         echo "WARNING, WAIT FOR THE FILE TO BE FULLY DOWNLOADED BEFORE YOU HIT ANY KEY"
@@ -255,11 +296,39 @@ do
     echo "Want to cleanup? ((y)es / (n)o)"
 	read answer
     if [ "$answer" == "yes" ] || [ "$answer" == "y" ]; then
-        sudo rm ~/Downloads/$scala_foldername.deb
-        sudo rm ~/Downloads/$scala_foldername.tgz
 
-        sudo rm ~/Downloads/eclipse-inst-jre-linux64.tar.gz
-        sudo rm ~/Downloads/jetbrains-toolbox-1.28.1.15219.tar.gz
+        cd ~/Downloads
+
+        if [ $(find -maxdepth 1 | grep $scala_foldername.deb) ]; then
+            sudo rm $scala_foldername.deb
+            echo "COPY OF SCALA.DEB DELETED. CONTINUING"
+        else
+            echo "SCALA.DEB FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+        
+        if [ $(find -maxdepth 1 | grep $scala_foldername.tgz) ]; then
+            sudo rm $scala_foldername.tgz
+            echo "COPY OF SCALA.TGZ DELETED. CONTINUING"
+        else
+            echo "SCALA.TGZ FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+
+        if [ $(find -maxdepth 1 | grep eclipse-inst-jre-linux) ]; then
+            sudo rm eclipse-inst-jre-linux64.tar.gz
+            echo "COPY OF ECLIPSE.TAR.GZ DELETED. CONTINUING"
+        else
+            echo "ECLIPSE.TAR.GZ FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+
+        if [ $(find -maxdepth 1 | grep jetbrains-toolbox) ]; then
+            sudo rm jetbrains-toolbox-1.28.1.15219.tar.gz
+            echo "COPY OF JETBRAINS-TOOLBOX DELETED. CONTINUING"
+        else
+            echo "JETBRAINS-TOOLBOX FILE NOT FOUND TO DELETE. CONTINUING"
+        fi
+        
+        cd ~
+
         break
 
     elif [ "$answer" == "no" ] || [ "$answer" == "n" ]; then
